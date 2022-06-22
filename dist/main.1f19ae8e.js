48975451b1ff124d3f78cf9da8ece62a37fe25f1
kKimMinChan <dfmj4520@naver.com>
2022-06-22 15:27:08 +0900

    2

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -172,6 +172,30 @@ spyEls.forEach(function (spyEl) {
   }).setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
   .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
 });
+var spyElss = document.querySelectorAll('.content__item.scroll-spy'); // 요소들 반복 처리!
+
+spyElss.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    // 감시할 장면(Scene)을 추가
+    triggerElement: spyEl,
+    // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: .99 // 화면의 90% 지점에서 보여짐 여부 감시
+
+  }).setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+  .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+});
+var spyElsss = document.querySelectorAll('.scroll-spy-box.scroll-spy'); // 요소들 반복 처리!
+
+spyElsss.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    // 감시할 장면(Scene)을 추가
+    triggerElement: spyEl,
+    // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: .9 // 화면의 90% 지점에서 보여짐 여부 감시
+
+  }).setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+  .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+});
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -200,7 +224,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49863" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54758" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
